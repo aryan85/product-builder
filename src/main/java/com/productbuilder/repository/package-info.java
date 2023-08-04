e45b318200526bce79fdcd40763c1473e74b1daf
ayan <aryan.scat@gmail.com>
2023-08-04 22:21:21 +0330

    Initial version of product-builder generated by generator-jhipster@8.0.0-beta.2

---
+++ src/main/java/com/productbuilder/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Repository layer.
+ */
+package com.productbuilder.repository;
---
+++ src/test/java/com/productbuilder/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.productbuilder.cucumber;
+
+import com.productbuilder.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
